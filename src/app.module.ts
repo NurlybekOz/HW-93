4fd79963c26855e5ddb95ad4add03d206aba9ed7
nurlybek <orozovnurlybek@gmail.com>
2025-05-23 22:46:59 +0600

    [HW-94] Добавил проверку на корректность пароля и наличие токена в запросе

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,10 @@ import { Album, AlbumSchema } from './schemas/album.schema';
 import { Track, TrackSchema } from './schemas/track.schema';
 import { AlbumsController } from './albums/albums.controller';
 import { TracksController } from './tracks/tracks.controller';
+import { User, UserSchema } from './schemas/user.schema';
+import { UsersController } from './users/users.controller';
+import { AuthService } from './auth/auth.service';
+import { LocalStrategy } from './local.strategy';
 
 @Module({
   imports: [
@@ -16,6 +20,7 @@ import { TracksController } from './tracks/tracks.controller';
       { name: Artist.name, schema: ArtistSchema },
       { name: Album.name, schema: AlbumSchema },
       { name: Track.name, schema: TrackSchema },
+      { name: User.name, schema: UserSchema },
     ]),
   ],
   controllers: [
@@ -23,7 +28,8 @@ import { TracksController } from './tracks/tracks.controller';
     ArtistsController,
     AlbumsController,
     TracksController,
+    UsersController,
   ],
-  providers: [AppService],
+  providers: [AppService, AuthService, LocalStrategy],
 })
 export class AppModule {}
