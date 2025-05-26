bc85733a2208a0056ed9459370050f440c3f2852
nurlybek <orozovnurlybek@gmail.com>
2025-05-26 19:56:33 +0600

    [HW-94] Добавил endpoints (login/register/logout)

---
+++ src/users/users.controller.ts
@@ -0,0 +1,37 @@
+import { Body, Controller, Delete, Post, Req, UseGuards } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from '../schemas/user.schema';
+import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
+import { RegisterUserDto } from './register-user.dto';
+import { TokenAuthGuard } from '../token-auth/token-auth.guard';
+
+@Controller('users')
+export class UsersController {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+  @Post()
+  register(@Body() registerUserDto: RegisterUserDto) {
+    const user = new this.userModel({
+      email: registerUserDto.email,
+      displayName: registerUserDto.displayName,
+      password: registerUserDto.password,
+      role: registerUserDto.role,
+    });
+
+    user.generateToken();
+    return user.save();
+  }
+
+  @UseGuards(AuthGuard('local'))
+  @Post('/sessions')
+  login(@Req() req: Request<{ user: User }>) {
+    return req.user;
+  }
+
+  @UseGuards(TokenAuthGuard)
+  @Delete('/sessions')
+  logout(@Req() req: Request) {
+    return { user: req.user, message: 'Success Logout' };
+  }
+}
